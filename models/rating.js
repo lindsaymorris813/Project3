81cc66eb48bd09008c3b9dc62627893994815fcf
valliappan Lakshmanan <valliappanlakshmanan@valliappans-MacBook-Air.local>
2021-01-21 13:02:35 -0500

    added in eslint

--- models/rating.js
+++ models/rating.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ratingSchema = new Schema({
-    recipeId: { type: Schema.Types.ObjectId, ref: "Recipe"},
-    rating: { type: Number },
-    dateOfRating: { type: Date }
+  recipeId: { type: Schema.Types.ObjectId, ref: "Recipe"},
+  rating: { type: Number },
+  dateOfRating: { type: Date }
 });
 
 
