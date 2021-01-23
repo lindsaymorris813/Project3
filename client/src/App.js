79ce6a3ac6b2e753a18a0e7e85f6abc92352f8b2
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-23 12:45:22 -0500

    added more paths to the app.js page so all pages can be viewed; added profile page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import SignUp from "./pages/SignUp.js";
 import Login from "./pages/Login.js";
+import Profile from "./pages/Profile.js";
 import Dashboard from "./pages/Dashboard.js";
 import AddRecipe from "./pages/AddRecipe.js";
 import SearchRecipe from "./pages/SearchRecipe.js";
@@ -13,9 +14,13 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <Route exact path="/" component={Login} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/recipecard" component={RecipeCard} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addrecipe" component={AddRecipe} />
+          <Route exact path="/searchrecipe" component={SearchRecipe} />
         </div>
       </Router>
     );
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function Profile (){
+
+
+
+    return(
+        <>
+
+        </> 
+
+)
+
+
+
+}
+export default Profile
