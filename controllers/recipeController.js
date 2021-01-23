f35e441c1683d9281b460dd0221123c635ec3e50
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-23 15:08:54 -0500

    controllers, routes

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -9,11 +9,33 @@ module.exports = {
       .catch(err=>res.status(422).json(err));
   },
   //route to return recipies based on search categories
-  searchRecipe:function(req,res){
+  searchRecipe: function(req,res){
     db.Recipe
-      .findAll()//have to identify how to add in search criteria here.
+      .findAll(req.query)//have to identify how to add in search criteria here.
+      .sort({ rating: -1 })
       .then(dbModel=>res.json(dbModel))
       .catch(err=>res.status(422).json(err));
+  },
+  //delete a recipe based on its ID
+  deleteRecipe: function(req, res) {
+    db.Recipe
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  //find a recipe based on its ID
+  findRecipe: function(req, res) {
+    db.Recipe
+      .findById({ _id: req.params.id })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  //update recipe based on its ID
+  updateRecipe: function(req, res) {
+    db.Recipe
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
-
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,19 @@
 const db = require("../models");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   //Login route
-  logIn: function(req,res){
+  logIn: function(req, res, next) {
+
 
   },
   //signUp route for creating new user
   signUp: function(req,res){
     db.User
       .create(req.body)
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
       .then(dbModel => res.json(dbModel))
       .catch(err=>res.status(422).json(err));
   },
