8c1aff030d86237892542cf08da311d66cdac0e5
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-24 12:29:19 -0500

    added nav item for left navigation box and updated header to only have welcome and logout

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,55 +10,9 @@ function Header() {
         <>
             <nav className="navbar navbar-light">
                 <div className="navbar-brand">Welcome!</div>
-                <div>
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link
-                                to="/profile"
-                                className={
-                                    window.location.pathname === "/profile" || window.location.pathname === "/about"
-                                        ? "nav-link active"
-                                        : "nav-link"
-                                }
-                            >Profile
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                to="/dashboard"
-                                className={
-                                    window.location.pathname === "/dashboard" || window.location.pathname === "/about"
-                                        ? "nav-link active"
-                                        : "nav-link"
-                                }
-                            >Dashboard
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                to="/searchrecipe"
-                                className={
-                                    window.location.pathname === "/searchrecipe" || window.location.pathname === "/about"
-                                        ? "nav-link active"
-                                        : "nav-link"
-                                }
-                            >Search Recipes
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                to="/addrecipe"
-                                className={
-                                    window.location.pathname === "/addrecipe" || window.location.pathname === "/about"
-                                        ? "nav-link active"
-                                        : "nav-link"
-                                }
-                            >Add Recipe
-                            </Link>
-                        </li>
-                        
-                    </ul>
-                </div>
+                <form class="form-inline">
+                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Logout</button>
+                </form>
             </nav>
         </>
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Nav from "../components/Nav";
 
 function Profile() {
 
@@ -9,8 +11,7 @@ function Profile() {
     return (
         <>
             <Header />
-
-
+            <Nav />
             <Footer />
         </>
 
