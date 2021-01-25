3bf4521146d1ec78fbfc32bb6d94824c94805b80
Star-Gibson <sgibsoncoding@gmail.com>
2021-01-25 15:37:04 -0500

    Authentication working fully.

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,23 +1,23 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
 
 function Jumbotron() {
 
-    return (
+  return (
         <>
             <div className="jumbotron jumbotron-fluid">
-                <div className="container-jumbo">
-                    <h1 className="display-2">Juicy Smoothie</h1>
-                    <p className="lead"><strong>Healthy smoothies at your fingertips!</strong></p>
+              <div className="container-jumbo">
+                <h1 className="display-2">Juicy Smoothie</h1>
+                <p className="lead"><strong>Healthy smoothies at your fingertips!</strong></p>
 
-                </div>
+              </div>
             </div>
         </>
 
-    )
+  );
 
 
 
 }
-export default Jumbotron 
+export default Jumbotron;
