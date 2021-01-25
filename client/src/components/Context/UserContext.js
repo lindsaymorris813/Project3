8fad52c2a8385391d1eca7f9bec55509354cea6d
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-25 16:33:25 -0500

    logout redirect to login screen

--- client/src/components/Context/UserContext.js
+++ client/src/components/Context/UserContext.js
@@ -3,6 +3,7 @@ import React from "react";
 const UserContext = React.createContext({
   email: "",
   onLogin: () => undefined,
+  onLogout: () => undefined
 });
 
 export default UserContext;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
@@ -8,56 +8,18 @@ import { Redirect } from "react-router-dom";
 import API from "../utils/API";
 
 function Dashboard() {
-    const { email } = useContext(UserContext);
-    const [recipeOfWeek, setRecipeOfWeek] = useState([]);
+  // const { email } = useContext(UserContext);
+  // const [recipeOfWeek, setRecipeOfWeek] = useState([]);
 
-    useEffect(() => {
-        loadROW();
-    });
+  // async function loadROW() {
+  //   await API.getROW()
+  //     .then(res => setRecipeOfWeek(res.data))
+  //     .catch(err => console.log(err));
+  // }
 
-    function loadROW() {
-        API.getROW()
-          .then(res => setRecipeOfWeek(res.data))
-          .catch(err => console.log(err));
-    };    
-
-    const renderDashboard = () => {
-        if (email) {
-            return (
-                <>
-                    <Header />
-                    <div className="row">
-                        <Nav />
-                        <div className="col-9">
-                            <div className="container">
-                                <div className="row shadow p-3 m-3 rounded list-border">
-                                    <div className="col-3">
-                                        <img className="smoothie" src="images/fruitTray.jpg" ></img>
-                                    </div>
-                                    <div className="col-9">
-                                        <h3>Dashboard heading. What else here?</h3>
-                                    </div>
-                                </div>
-                                <div className="row ">
-                                    <div className="col shadow p-3 m-3 rounded list-border">
-                                        <div className="container">
-                                            <h2>Smoothie of the Week</h2>
-                                            <h4>{recipeOfWeek[0].title}</h4>
-                                            <p>{recipeOfWeek[0].rating} out of 5 stars!</p>
-                                            <img src= {recipeOfWeek[0].image} alt={recipeOfWeek[0].title}/>
-                                            <p>{recipeOfWeek[0].prep}</p>
-                                        </div>
-                                </div>
-                                    <div className="col shadow p-3 m-3 rounded list-border">
-                                        My Recipes
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-  const { email } = useContext(UserContext);
-  console.log(email);
+  // useEffect(() => {
+  //   loadROW();
+  // });
 
   return (
     <>
@@ -76,7 +38,9 @@ function Dashboard() {
             </div>
             <div className="row ">
               <div className="col shadow p-3 m-3 rounded list-border">
-                Smoothie of the week
+                <div className="container">
+                  <h2>Smoothie of the Week</h2>
+                </div>
               </div>
               <div className="col shadow p-3 m-3 rounded list-border">
                 My Recipes
