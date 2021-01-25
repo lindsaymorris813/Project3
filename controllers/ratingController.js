8a000987aa9c525c9aa85ba611a991259d0911d2
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-25 09:55:40 -0500

    rating controller, updated getROW, getRating, updateRating

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -4,14 +4,14 @@ module.exports = {
   //route for getting smoothy of the week
   getROW: function(req, res){
     Rating
-      .aggregate([{$group: {_id: "$recipeId", avgRating: {$avg: "$rating"}}}, {$sort: { avgRating: -1 }}])
-      .sort({ $avgRating: -1 })
+      .aggregate([{$match: { dateOfRating: {$gt: new Date(Date.now() - 12096e5)}}},
+        {$group: {_id: "$recipeId", avgRating: {$avg: "$rating"}}}, {$sort: { avgRating: -1 }}])
       .then(dbModel=>resJson(dbModel))
       .catch(err=>res.status(422).json(err));
   },
   getRating: function(req, res){
     Rating
-      .aggregate([{$group: {_id: req.params.id, avg: {$avg:"$rating"}}}])
+      .aggregate([{$group: {_id: "$recipeId", avgRating: {$avg:"$rating"}}}])
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
@@ -23,7 +23,7 @@ module.exports = {
   },
   updateRating: function(req, res){
     Rating
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .findOneAndUpdate({ _id: {where: { recipeId: req.params.id, userId: req.user.id }}}, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
