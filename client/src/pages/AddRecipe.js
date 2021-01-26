4fd1bc38e839a268658ccee56faddb0ff2ea6757
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-25 19:33:16 -0500

    corrected links to the new globalstyles.css file

--- client/src/pages/AddRecipe.js
+++ client/src/pages/AddRecipe.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
