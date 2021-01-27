1b1033b53a871513945c8df7021517ab99fcd339
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-26 22:25:40 -0500

    weeek

--- client/src/pages/SearchRecipe.js
+++ client/src/pages/SearchRecipe.js
@@ -1,33 +1,36 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
-import RecipeCard from "../components/RecipeCard";
+import SearchRecipeCard from "../components/SearchRecipeCard";
 import Nav from "../components/Nav";
 import API from "../utils/API";
 
 function SearchRecipe() {
-  // const [queryCategory, setQueryCategory] = useState({
-  //   lowSugar: false,
-  //   highProtein: false,
-  //   lowCarb: false,
-  //   weightLoss: false
-  // });
+  const [queryCategory, setQueryCategory] = useState({
+    lowSugar: false,
+    highProtein: false,
+    lowCarb: false,
+    weightLoss: false
+  });
 
-  // const [searchBar, setSearchBar] = useState([]);
+  const [foundRecipes, setFoundRecipes] = useState([]);
+  const [searchBar, setSearchBar] = useState();
 
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-  //   if (searchBar) {
-  //     API.searchRecipes(searchBar)
-  //       .then((res) => console.log(res.data))
-  //       .catch(err => console.log(err));
-  //   }
-  // }
+  function recipeSearch(query) {
+    API.searchRecipes(query)
+      .then((res) => console.log(res))
+      .catch(err => console.log(err));
+  }
 
-  // function handleInputChange(event) {
-  //   const { value } = event.target;
-  //   setSearchBar(value);
-  // }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(searchBar);
+    recipeSearch(searchBar);
+  }
+
+  function handleInputChange(event) {
+    const { value } = event.target;
+    setSearchBar(value);
+  }
 
   // const handleCheckClick = (id) => {
   //   switch (id) {
@@ -64,16 +67,23 @@ function SearchRecipe() {
         <div className="col-9">
           <div className="container">
             <div className="row"></div>
-            <div className="row shadow p-3 m-3 rounded list-border">
+            <div className="row shadow p-3 m-3 rounded list-border page-header">
               <div className="col-1"></div>
               <div className="col-10">
-                {/* <form className="login">
+                <form className="login">
+                  <div className="form-group">
+                    <label htmlFor="searchRecipe"><h2><strong>Search for a Recipe</strong></h2></label>
+                    <input type="text" className="form-control shadow p-3 m-3 bg-white rounded" id="recipe-search"
+                      placeholder="Search Input"></input>
+                  </div>
+                </form>
+                {<form className="login">
                   <div className="form-group">
                     <label htmlFor="searchRecipe"><h2>Search for a Recipe</h2></label>
                     <input type="text" className="form-control shadow p-3 m-3 bg-white rounded" id="recipe-search"
                       placeholder="Search Input" onChange={handleInputChange}></input>
                   </div>
-                  <div className="form-check form-check-inline">
+                  {/* <div className="form-check form-check-inline">
                     <input className="form-check-input" type="checkbox" id="lowSugar" value="Low Sugar" click={handleCheckClick("lowSugar")}></input>
                     <label className="form-check-label" htmlFor="inlineCheckbox1">Low Sugar</label>
                   </div>
@@ -88,30 +98,29 @@ function SearchRecipe() {
                   <div className="form-check form-check-inline">
                     <input className="form-check-input" type="checkbox" id="weightLoss" value="Weight Loss" click={handleCheckClick("weightLoss")}></input>
                     <label className="form-check-label" htmlFor="inlineCheckbox2">Weight Loss</label>
-                  </div>
-
+                  </div>*/}
                   <div className="row">
                     <div className="col-12 text-center clearfix">
                       <button type="submit" className="btn btn-dark active float-right" id="search-btn" onClick={handleSubmit}>Submit</button>
                     </div>
-                  </div>
-                </form> */}
+                  </div> 
+                </form>
               </div>
               <div className="col-1"></div>
             </div>
-            <div className="row shadow p-3 m-3 rounded list-border">
+            <div className="row shadow p-3 m-3 rounded list-border recipe-list">
               <div className="col-1"></div>
               <div className="col-10">
-                <RecipeCard key="a"/>
-                <RecipeCard key="b"/>
-                <RecipeCard key="c"/>
+                <h2><strong>Search Results</strong></h2>
+                <SearchRecipeCard key="a"/>
+                <SearchRecipeCard key="b"/>
+                <SearchRecipeCard key="c"/>
               </div>
               <div className="col-1"></div>
             </div>
           </div>
         </div>
       </div>
-      <Footer />
     </>
 
   );
